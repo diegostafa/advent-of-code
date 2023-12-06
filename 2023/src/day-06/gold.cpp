6917be157f058b02fc0b10e7799c4294c630625e
diegostafa <dstafa.dev@gmail.com>
2023-12-06 06:43:30 +0100

    [2023] day 6

---
+++ 2023/src/day-06/gold.cpp
@@ -0,0 +1,30 @@
+#include "../../utils/aoc-utils.cpp"
+
+auto split_space = [](const auto& s)
+{ return aoc_utils::split_string(s, " "); };
+
+auto to_num = [](const auto& s)
+{ return (int64_t)std::stoll((s.c_str())); };
+
+int
+main()
+{
+    auto parse_line = [](const auto& s)
+    { return split_space(s) | std::views::drop(1) | std::views::transform(to_num); };
+
+    auto input     = aoc_utils::read_file_lines("src/day-06/input.txt") | std::views::transform(parse_line);
+    auto times     = input[0];
+    auto distances = input[1];
+
+    auto compute_better_time = [](auto time, auto dist)
+    {
+        int64_t better_results = 0;
+        for (int64_t speed = 0; speed < time; speed++)
+            if (speed * (time - speed) > dist)
+                better_results++;
+
+        return better_results;
+    };
+
+    std::cout << compute_better_time(times[0], distances[0]) << std::endl;
+}
---
+++ 2023/src/day-06/silver.cpp
@@ -0,0 +1,34 @@
+#include "../../utils/aoc-utils.cpp"
+
+auto split_space = [](const auto& s)
+{ return aoc_utils::split_string(s, " "); };
+
+auto to_num = [](const auto& s)
+{ return (int64_t)std::stoll((s.c_str())); };
+
+int
+main()
+{
+    auto parse_line = [](const auto& s)
+    { return split_space(s) | std::views::drop(1) | std::views::transform(to_num); };
+
+    auto input     = aoc_utils::read_file_lines("src/day-06/input.txt") | std::views::transform(parse_line);
+    auto times     = input[0];
+    auto distances = input[1];
+
+    auto compute_better_time = [](auto time, auto dist)
+    {
+        int64_t better_results = 0;
+        for (int64_t speed = 0; speed < time; speed++)
+            if (speed * (time - speed) > dist)
+                better_results++;
+
+        return better_results;
+    };
+
+    int64_t sum = 1;
+    for (int64_t i = 0; i < times.size(); i++)
+        sum *= compute_better_time(times[i], distances[i]);
+
+    std::cout << sum << std::endl;
+}

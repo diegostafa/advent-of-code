d9a0db284fbffa1c70389b21c76b30371ce89aa1
diegostafa <dstafa.dev@gmail.com>
2024-12-09 18:57:01 +0100

    [2024] day 9

--- aoc-24/src/day9.rs
+++ aoc-24/src/day9.rs
@@ -12,15 +12,21 @@ pub enum NodeType {
 }
 impl NodeType {
     pub fn is_file(&self) -> bool {
-        matches!(self, NodeType::File(_, _))
+        matches!(self, NodeType::File(..))
     }
     pub fn is_space(&self) -> bool {
-        matches!(self, NodeType::Space(_))
+        matches!(self, NodeType::Space(..))
     }
     pub fn size(&self) -> i32 {
         match self {
             NodeType::File(_, size) => *size,
             NodeType::Space(size) => *size,
+        }
+    }
+    pub fn id(&self) -> i32 {
+        match self {
+            NodeType::File(id, _) => *id,
+            NodeType::Space(_) => -1,
         }
     }
 }
@@ -42,32 +48,20 @@ pub fn input_generator(input: &str) -> Input {
 }
 
 pub fn decompress(input: &Vec<NodeType>) -> Vec<i32> {
-    let mut data = vec![];
-    for node in input {
-        match node {
-            NodeType::File(id, size) => {
-                for _ in 0..*size {
-                    data.push(*id);
-                }
-            }
-            NodeType::Space(size) => {
-                for _ in 0..*size {
-                    data.push(-1);
-                }
-            }
-        }
+    let size = input.iter().fold(0, |acc, node| acc + node.size() as usize);
+    let mut data = Vec::with_capacity(size);
+    for n in input {
+        let id = n.id();
+        (0..n.size()).for_each(|_| data.push(id));
     }
     data
 }
 
 pub fn checksum(data: Vec<i32>) -> u64 {
-    let mut sum = 0;
-    for (i, id) in data.into_iter().enumerate() {
-        if id != -1 {
-            sum += (i as u64) * (id as u64);
-        }
-    }
-    sum
+    data.into_iter()
+        .enumerate()
+        .filter(|(_, id)| *id != -1)
+        .fold(0, |acc, (i, id)| acc + (i as u64) * (id as u64))
 }
 
 pub fn part1(input: &Input) -> u64 {
@@ -82,15 +76,13 @@ pub fn part2(input: &Input) -> u64 {
     let mut data = input.clone();
     let mut j = data.len() - 1;
     while j > 0 {
-        if let Some(i) = data
+        if let Some(i) = data[..j]
             .iter()
             .position(|n| n.is_space() && data[j].is_file() && n.size() >= data[j].size())
         {
-            if i < j {
-                data[i] = NodeType::Space(data[i].size() - data[j].size());
-                data.insert(i, data[j]);
-                data[j + 1] = NodeType::Space(data[j + 1].size());
-            }
+            data[i] = NodeType::Space(data[i].size() - data[j].size());
+            data.insert(i, data[j]);
+            data[j + 1] = NodeType::Space(data[j + 1].size());
         }
         j -= 1;
     }
